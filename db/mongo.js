1892825689d996ec54c87d2b307aabf5cc065c70
Roshan <rxgody@gmail.com>
2021-12-20 20:10:13 +0530

    start package changed

---
+++ db/mongo.js
@@ -0,0 +1,17 @@
+require('./db/mongoose.js')
+const express=require('express')
+const { Mongoose }= require('mongoose')
+const app=express()
+const bodyParser=require('body-parser')
+const Todo=require('./models/todo')
+const path=require('path')
+
+const bcrypt=require('bcryptjs')
+
+app.use(bodyParser.json())
+
+app.get('/',async (req,res)=>{
+    const data=await Todo.find({name:'rosh'})
+    console.log(data)
+    res.json(data)
+})
